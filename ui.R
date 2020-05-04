996497596061b5082bdbaa1e714df4c8b6cb0de2
shorty0789 <63329808+shorty0789@users.noreply.github.com>
2020-05-04 16:18:40 -0400

    home health

--- ui.R
+++ ui.R
@@ -3,26 +3,57 @@ library(shinydashboard)
 
 dashboardPage(
     dashboardHeader(
-    title = "Mastering RStudio - Water Dashboard",
+    title = "Mastering RStudio - Home Help Dashboard",
     titleWidth = 350
     ),
     dashboardSidebar(
       sidebarMenu(
-        menuItem("Main KPI overview",
+        menuItem("Main Home Help overview",
                  tabName = "mainDashboard",
-                 icon = icon("dashboard"))
+                 icon = icon("dashboard")),
+        
+        menuItem("Google Analytics Dashboard",
+                 tabName = "googleAnalytics",
+                 icon = icon("google")),
+        
+        menuItem("Customer Dashboard",
+                 tabName = "customerDashboard",
+                 icon = icon("users"))
       )
     ),
     dashboardBody(
       tabItems(
         tabItem(tabName = "mainDashboard",
-                h2 ("Main KPI Dashboard"),
+               
+                 h2 ("Main Home Help Dashboard"),
+                
+                fluidRow(
+                  box(
+                    "Box 1 content",
+                    br(),
+                    "More box content"
+                  ),
+                )
+                
+                fluidRow(
+                box(
+                  "Box 2 content",
+                  br(),
+                  "Even More Box Content"
+                ),
                 
                 box(
-                  "Box 1 content",
+                  "Box 3 content",
                   br(),
-                  "More Box Content"
-                ))
+                  "Even More Box Content"
+                ),
+                
+                box(
+                  "Box 4 content",
+                  br(),
+                  "Even More Box Content"
+                )
+            )
       )
     ),
 )
